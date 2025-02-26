f3ae9680d4209270f95677a5dd72200e4899fa6b
AllisonJiao <112785687+AllisonJiao@users.noreply.github.com>
2025-02-25 18:48:26 -0800

    Removed pem for security

--- index.ts
+++ index.ts
@@ -45,8 +45,8 @@ Routes.forEach((route) => {
 
 // Load self-signed certificate and private key
 const options = {
-  key: fs.readFileSync('/etc/ssl/private/server.key'), // Path to your private key
-  cert: fs.readFileSync('/etc/ssl/certs/server.crt')   // Path to your certificate
+  key: fs.readFileSync('/etc/ssl/private/server.key'),
+  cert: fs.readFileSync('/etc/ssl/certs/server.crt')
 };
 
 connectDB().then(() => {
